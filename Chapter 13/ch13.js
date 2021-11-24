6200f79c4ec31491cb4bff2d8c4ac490908a5f67
brightboost <info@brightboost.nl>
2021-11-24 19:42:15 +0100

    reorganize

--- Chapter 13/ch13.js
+++
@@ -1,6 +0,0 @@
-let varContainingFunction = function () {
-  let varInFunction = "I'm in a function.";
-  console.log("hi there!");
-};
-
-varContainingFunction();
--- Chapter 13/concurrency_chapter/eventloop-sync.js
+++
@@ -1,6 +0,0 @@
-console.log("Hi there");
-add(4,5);
-
-function add(x, y) {
-    return x + y;
-}
