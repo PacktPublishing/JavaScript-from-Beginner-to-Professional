7720c1c870837d6fd3d97e80be2bf31e79fb0bfd
Karan <50290386+Sonawane-Karan26@users.noreply.github.com>
2021-12-03 14:10:06 +0530

    restructured

--- Chapter 08/Exercise_8.2.js
+++ Chapter 08/Exercise_8.2.js
@@ -1,6 +1,6 @@
 const arr = ["Laurence", "Mike", "Larry", "Kim", "Joanne", "Laurence", "Mike", "Laurence", "Mike", "Laurence", "Mike"];
 const arr2 = arr.filter ( (value, index, array) => {
     console.log(value,index,array.indexOf(value));
-    return array.indexOf(value) === index
+    return array.indexOf(value) === index;
 });
 console.log(arr2);
