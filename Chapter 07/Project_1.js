c5f161fc291272ea6332768eddbb205721f70ad4
Karan <50290386+Sonawane-Karan26@users.noreply.github.com>
2021-12-03 14:09:36 +0530

    restructured

--- Chapter 07/Project_1.js
+++ Chapter 07/Project_1.js
@@ -10,9 +10,9 @@ const person2 = new Employee("Jane", "Doe", 5);
 const workers = [person1, person2];
 
 Employee.prototype.details = function(){
-    return this.first + " " + this.last + " has worked here " + this.years + " years"
+    return this.first + " " + this.last + " has worked here " + this.years + " years";
 }
 
 workers.forEach((person) => {
     console.log(person.details());
-})
+});
