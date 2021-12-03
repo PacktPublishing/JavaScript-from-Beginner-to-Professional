9b71a4255453907bf8677987b1bfefc5bb543493
Karan <50290386+Sonawane-Karan26@users.noreply.github.com>
2021-12-03 14:04:50 +0530

    restructured

--- Chapter 02/Project_1.js
+++ Chapter 02/Project_1.js
@@ -2,4 +2,4 @@
 //1 mile equals 1.60934 kilometers.
 let myDistanceMiles = 130;
 let myDistanceKM = myDistanceMiles * 1.60934;
-console.log("The distance of " + myDistanceMiles + " miles is equal to " + myDistanceKM + " kilometers")
+console.log("The distance of " + myDistanceMiles + " miles is equal to " + myDistanceKM + " kilometers");
